af65c90c3149a2a267d499ca748784025a80d7fd
Redleafz2735 <petmankak@gmail.com>
2022-08-29 17:20:24 +0700

    git init

---
+++ lib/main.dart
@@ -0,0 +1,138 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String result = "0";
+  String expression = "";
+
+  buttonPressed(String value) {
+    print(value);
+    setState(() {
+      if (value == "CLEAR") {
+        result = "0";
+      } else if (value == ".") {
+        if (result.contains(".")) {
+          return;
+        } else {
+          result = result + value;
+        }
+      } else if (value == "=") {
+        expression = result.replaceAll("X", "*");
+
+        Parser p = Parser();
+        Expression exp = p.parse(expression);
+
+        ContextModel cm = ContextModel();
+        dynamic calculate = exp.evaluate(EvaluationType.REAL, cm);
+
+        result = "$calculate";
+      } else {
+        if (result == "0") {
+          result = value;
+        } else {
+          result = result + value;
+        }
+      }
+    });
+  }
+
+  Widget myButton(String buttonLabel) {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: OutlinedButton(
+            onPressed: () => buttonPressed(buttonLabel),
+            child: Text(
+              buttonLabel,
+              style: TextStyle(fontSize: 20),
+            )),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 211, 230, 250),
+        appBar: AppBar(
+          title: Text("Calculator"),
+        ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 24),
+                alignment: Alignment.centerRight,
+                child: Text(
+                  result,
+                  style: TextStyle(
+                      fontSize: 48,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 255, 0, 0)),
+                ),
+              ),
+              Expanded(
+                child: Divider(),
+              ),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      myButton('7'),
+                      myButton("8"),
+                      myButton("9"),
+                      myButton("/"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      myButton("4"),
+                      myButton("5"),
+                      myButton("6"),
+                      myButton("X"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      myButton("1"),
+                      myButton("2"),
+                      myButton("3"),
+                      myButton("-"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      myButton("."),
+                      myButton("0"),
+                      myButton("00"),
+                      myButton("+"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      myButton("CLEAR"),
+                      myButton("="),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
